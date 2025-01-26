8f979ae033f0d34208d3b9d8ed214210a78a5939
XxAnthonyuwuxX <paraderpara@gmail.com>
2025-01-26 15:49:09 -0500

    Implementacion de la funcion "agregarAmigoLista"

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ problema.*/
 let amigos = [];
 
 function agregarAmigo(){
-    let amigo = (document.getElementById('amigo'));
+    let amigo = document.getElementById('amigo');
     if (amigo.value === ''){
         return alert("Por favor, inserte un nombre.")
     }else{
         amigos.push(amigo.value);
         amigo.value = '';
+        agregarAmigoLista();
         return console.log(amigos);
     }
-};
+}
+
+function agregarAmigoLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++){
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
