90a90de4236fbce36f78423a0a3915f62423eec0
XxAnthonyuwuxX <paraderpara@gmail.com>
2025-01-26 03:08:28 -0500

    Se agrego la funcion 'agregarAmigo' en app.js

--- app.js
+++ app.js
@@ -2,3 +2,16 @@
 fortalecer tus habilidades en lógica de programación. 
 Aquí deberás desarrollar la lógica para resolver el
 problema.*/
+
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo = (document.getElementById('amigo'));
+    if (amigo.value === ''){
+        return alert("Por favor, inserte un nombre.")
+    }else{
+        amigos.push(amigo.value);
+        amigo.value = '';
+        return console.log(amigos);
+    }
+};
